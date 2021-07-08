2f51bdb5a6e4c785fc1e0dc227a78100dae135dd
Paulo Eduardo <paulo.edhuardo@hotmail.com>
2021-07-08 11:15:12 -0300

    adding reports base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule)},
-  {path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule)}
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule) },
+  { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) }
 ];
 
 @NgModule({
